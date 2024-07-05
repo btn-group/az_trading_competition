3c61960ae3c3a1913db94981da08571584460a93
Steven Chang <stevenchang5000@protonmail.com>
2024-07-05 14:53:16 +1000

    fn create

--- lib.rs
+++ lib.rs
@@ -15,6 +15,10 @@ mod az_trading_competition {
     // === TYPES ===
     type Result<T> = core::result::Result<T, AzTradingCompetitionError>;
 
+    // === CONSTANTS ===
+    // Minimum 1 hour
+    const MINIMUM_DURATION: Timestamp = 3_600_000;
+
     // === STRUCTS ===
     #[derive(Debug, Clone, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
@@ -47,6 +51,7 @@ mod az_trading_competition {
         competition_allowed_pools: Mapping<(u64, AccountId), bool>,
         competition_token_users: Mapping<(u64, AccountId, AccountId), Balance>,
         competition_count: u64,
+        competitions: Mapping<u64, Competition>,
         oracle: AccountId,
     }
     impl AzTradingCompetition {
@@ -58,6 +63,7 @@ mod az_trading_competition {
                 competition_allowed_pools: Mapping::default(),
                 competition_token_users: Mapping::default(),
                 competition_count: 0,
+                competitions: Mapping::default(),
                 oracle,
             }
         }
@@ -71,6 +77,46 @@ mod az_trading_competition {
                 competition_count: self.competition_count,
                 oracle: self.oracle,
             }
+        }
+
+        // === HANDLES ===
+        #[ink(message)]
+        pub fn create(
+            &mut self,
+            start: Timestamp,
+            end: Timestamp,
+            entry_fee_token: AccountId,
+            entry_fee_amount: Balance,
+        ) -> Result<Competition> {
+            if self.competition_count == u64::MAX {
+                return Err(AzTradingCompetitionError::UnprocessableEntity(
+                    "Max number of tournaments reached.".to_string(),
+                ));
+            }
+            if end < start + MINIMUM_DURATION {
+                return Err(AzTradingCompetitionError::UnprocessableEntity(format!(
+                    "Tournament must run a minimum duration of {MINIMUM_DURATION}ms."
+                )));
+            }
+            if entry_fee_amount == 0 {
+                return Err(AzTradingCompetitionError::UnprocessableEntity(
+                    "Entry fee amount must be positive".to_string(),
+                ));
+            }
+
+            let competition: Competition = Competition {
+                id: self.competition_count,
+                start,
+                end,
+                entry_fee_token,
+                entry_fee_amount,
+                allowed_pools_vec: vec![],
+            };
+            self.competitions
+                .insert(self.competition_count, &competition);
+            self.competition_count += 1;
+
+            Ok(competition)
         }
     }
 
@@ -120,6 +166,82 @@ mod az_trading_competition {
             assert_eq!(config.admin, az_trading_competition.admin);
             assert_eq!(config.router, az_trading_competition.router);
             assert_eq!(config.oracle, mock_oracle_address());
+        }
+
+        // === TEST HANDLES ===
+        #[ink::test]
+        fn test_create() {
+            let (_accounts, mut az_trading_competition) = init();
+
+            // when competition_count is u64 max
+            az_trading_competition.competition_count = u64::MAX;
+            // * it raises an error
+            let result = az_trading_competition.create(
+                MOCK_START,
+                MOCK_END,
+                mock_entry_fee_token(),
+                MOCK_ENTRY_FEE_AMOUNT,
+            );
+            assert_eq!(
+                result,
+                Err(AzTradingCompetitionError::UnprocessableEntity(
+                    "Max number of tournaments reached.".to_string(),
+                ))
+            );
+            // when competition_count is less than u64 max
+            az_trading_competition.competition_count = u64::MAX - 1;
+            // = when duration is less than or equal to MINIMUM_DURATION
+            let result = az_trading_competition.create(
+                MOCK_START,
+                MOCK_START + MINIMUM_DURATION - 1,
+                mock_entry_fee_token(),
+                MOCK_ENTRY_FEE_AMOUNT,
+            );
+            // = * it raises an error
+            assert_eq!(
+                result,
+                Err(AzTradingCompetitionError::UnprocessableEntity(format!(
+                    "Tournament must run a minimum duration of {MINIMUM_DURATION}ms."
+                )))
+            );
+            // = when duration is greater than MINIMUM_DURATION
+            // == when fee amount is zero
+            let result = az_trading_competition.create(
+                MOCK_START,
+                MOCK_START + MINIMUM_DURATION,
+                mock_entry_fee_token(),
+                0,
+            );
+            // == * it raises an error
+            assert_eq!(
+                result,
+                Err(AzTradingCompetitionError::UnprocessableEntity(
+                    "Entry fee amount must be positive".to_string()
+                ))
+            );
+            // == when fee amount is positive
+            let competition_count: u64 = az_trading_competition.competition_count;
+            az_trading_competition
+                .create(
+                    MOCK_START,
+                    MOCK_START + MINIMUM_DURATION,
+                    mock_entry_fee_token(),
+                    MOCK_ENTRY_FEE_AMOUNT,
+                )
+                .unwrap();
+            // == * it stores the competition
+            assert_eq!(
+                az_trading_competition
+                    .competitions
+                    .get(&competition_count)
+                    .is_some(),
+                true
+            );
+            // == * it increases the competition_count by 1
+            assert_eq!(
+                az_trading_competition.competition_count,
+                competition_count + 1
+            );
         }
     }
 }
